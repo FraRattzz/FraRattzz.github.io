0e0f58afe1a90c70a5b3cd431e08e874cbe2f81e
JEANMONNET\ratti_francesco08 <fra.rat.11@gmail.com>
2026-04-15 13:29:20 +0200

    Esercizio Da Finire Quiz

---
+++ Quiz/script.js
@@ -0,0 +1,23 @@
+let mesi = ["gen", "feb", "mar", "apr", "mag", "giu", "lug", "ago", "set", "ott", "nov", "dic"];
+
+let risposte=["SQL","2","St"]
+function riempiMesi(){
+    let s ="";
+    for(let i=0;i<mesi.length;i++){
+        s+="<option>"+mesi[i] +"</option>";
+    }
+    document.getElementById("txtMese").innerHTML=s;
+
+}
+
+
+function elaboraDati(){
+    let punteggio=0;
+    let rispsta1=document.getElementById("risp1").value;
+    let risposta2=document.getElementById("txtRisp2").value;
+    let rispota3;
+    if(document.getElementById("radioS").value==true)
+
+}
+riempiMesi()
+
